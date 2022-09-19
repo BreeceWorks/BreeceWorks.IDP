f51156e7dabf1d309dd58e62e208ca0efa487e5f
Benjamin Breece <benjaminbreece@yahoo.com>
2022-09-19 14:10:21 -0500

    Finished Tutorial

--- BreeceWorks.IDP/Config.cs
+++ BreeceWorks.IDP/Config.cs
@@ -12,31 +12,46 @@ namespace BreeceWorks.IDP
     {
         public static IEnumerable<IdentityResource> Ids =>
             new IdentityResource[]
-            { 
+            {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
                 new IdentityResources.Address(),
                 new IdentityResource(
                     "roles",
                     "Your role(s)",
-                    new List<string>() { "role" })
+                    new List<string>() { "role" }),
+                new IdentityResource(
+                    "country",
+                    "The country you're living in",
+                    new List<string>() { "country" }),
+                new IdentityResource(
+                    "subscriptionlevel",
+                    "Your subscription level",
+                    new List<string>() { "subscriptionlevel" })
             };
 
         public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
+            new ApiResource[]
             {
                 new ApiResource(
-                    "imagegalleryapi", 
+                    "imagegalleryapi",
                     "Image Gallery API",
                     new List<string>() { "role" })
+                {
+                    ApiSecrets = { new Secret("apisecret".Sha256()) }
+                }
             };
-        
+
         public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { 
+            new Client[]
+            {
                 new Client
                 {
-                    ClientName = "Image Gallery", 
+                    AccessTokenType = AccessTokenType.Reference,
+                    AccessTokenLifetime = 120,
+                    AllowOfflineAccess = true,
+                    UpdateAccessTokenClaimsOnRefresh = true,
+                    ClientName = "Image Gallery",
                     ClientId = "imagegalleryclient",
                     AllowedGrantTypes = GrantTypes.Code,
                     RequirePkce = true,
@@ -48,18 +63,20 @@ namespace BreeceWorks.IDP
                     {
                         "https://localhost:44389/signout-callback-oidc"
                     },
-                    AllowedScopes = 
+                    AllowedScopes =
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
                         IdentityServerConstants.StandardScopes.Address,
                         "roles",
-                        "imagegalleryapi"
+                        "imagegalleryapi",
+                        "country",
+                        "subscriptionlevel"
                     },
                     ClientSecrets =
                     {
                         new Secret("secret".Sha256())
                     }
-                } };        
+                } };
     }
 }
--- BreeceWorks.IDP/Startup.cs
+++ BreeceWorks.IDP/Startup.cs
@@ -2,10 +2,15 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
+using System.Reflection;
 
 namespace BreeceWorks.IDP
 {
@@ -20,18 +25,44 @@ namespace BreeceWorks.IDP
 
         public void ConfigureServices(IServiceCollection services)
         {
+            var breeceWorksIDPDataDBConnectionString =
+    "Data Source=DESKTOP-0BPCUCG;Initial Catalog=BreeceWorksIDP;Integrated Security=true;MultipleActiveResultSets=True;";
+
+
             // uncomment, if you want to add an MVC-based UI
             services.AddControllersWithViews();
 
             var builder = services.AddIdentityServer()
-                .AddInMemoryIdentityResources(Config.Ids)
-                .AddInMemoryApiResources(Config.Apis)
-                .AddInMemoryClients(Config.Clients)
+                //.AddInMemoryIdentityResources(Config.Ids)
+                //.AddInMemoryApiResources(Config.Apis)
+                //.AddInMemoryClients(Config.Clients)
                 .AddTestUsers(TestUsers.Users);
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
+
+
+            var migrationsAssembly = typeof(Startup)
+                .GetTypeInfo().Assembly.GetName().Name;
+
+
+            builder.AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseSqlServer(breeceWorksIDPDataDBConnectionString,
+                    options => options.MigrationsAssembly(migrationsAssembly));
+            });
+
+            builder.AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseSqlServer(breeceWorksIDPDataDBConnectionString,
+                    options => options.MigrationsAssembly(migrationsAssembly));
+            });
+
         }
+
+
 
         public void Configure(IApplicationBuilder app)
         {
@@ -39,6 +70,8 @@ namespace BreeceWorks.IDP
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            InitializeDatabase(app);
 
             // uncomment if you want to add MVC
             app.UseStaticFiles();
@@ -53,5 +86,46 @@ namespace BreeceWorks.IDP
                 endpoints.MapDefaultControllerRoute();
             });
         }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider
+                    .GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider
+                    .GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.Ids)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.Apis)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+
     }
 }
